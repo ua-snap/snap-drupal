255a076456ca685fbff435df9c4d075eddecc6c8
Bruce Crevensten <bruce.crevensten@gmail.com>
2014-03-17 14:31:05 -0800

    interim commit working to implement mockup

---
+++ themes/snap/templates/page--front.tpl.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+<div id="homeIntroPanel">
+  <img src="<?php print drupal_get_path('theme', 'snap'); ?>/images/snap_color_lato.svg"/>
+  <h1>Exploring our future in a changing Arctic</h1>
+</div>
+
+<header class="header" id="header" role="banner">
+    <a id="navHomeLogo" href="/"><img src="<?php print drupal_get_path('theme', 'snap'); ?>/images/SNAP_acronym_color_lato.svg"/></a>
+    <?php print render($page['header']); ?>
+</header>
+
+<div id="pageWrapper">
+
+  <div id="page">
+
+    <div id="main">
+
+      <div id="content" class="column" role="main">
+        <?php print render($page['highlighted']); ?>
+       
+        <a id="main-content"></a>
+       
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      </div>
+
+      <div id="navigation">
+
+        <?php if ($main_menu): ?>
+          <nav id="main-menu" role="navigation" tabindex="-1">
+            <?php
+            // This code snippet is hard to modify. We recommend turning off the
+            // "Main menu" on your sub-theme's settings form, deleting this PHP
+            // code block, and, instead, using the "Menu block" module.
+            // @see https://drupal.org/project/menu_block
+            print theme('links__system_main_menu', array(
+              'links' => $main_menu,
+              'attributes' => array(
+                'class' => array('links', 'inline', 'clearfix'),
+              ),
+              'heading' => array(
+                'text' => t('Main menu'),
+                'level' => 'h2',
+                'class' => array('element-invisible'),
+              ),
+            )); ?>
+          </nav>
+        <?php endif; ?>
+
+        <?php print render($page['navigation']); ?>
+
+      </div>
+
+      <?php
+        // Render the sidebars to see if there's anything in them.
+        $sidebar_first  = render($page['sidebar_first']);
+        $sidebar_second = render($page['sidebar_second']);
+      ?>
+
+      <?php if ($sidebar_first || $sidebar_second): ?>
+        <aside class="sidebars">
+          <?php print $sidebar_first; ?>
+          <?php print $sidebar_second; ?>
+        </aside>
+      <?php endif; ?>
+
+    </div>
+
+    <?php print render($page['footer']); ?>
+
+  </div>
+
+<?php print render($page['bottom']); ?>
+
+    <?php if ($secondary_menu): ?>
+      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+        <?php print theme('links__system_secondary_menu', array(
+          'links' => $secondary_menu,
+          'attributes' => array(
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => $secondary_menu_heading,
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+      </nav>
+    <?php endif; ?>
+
+</div>
+
