917407d5ac90eef72a8540309a8e061bcfa2d1e0
Bruce Crevensten <bruce.crevensten@gmail.com>
2014-04-02 11:08:22 -0800

    styling, enable display-type templates

--- themes/snap_bootstrap/template.php
+++ themes/snap_bootstrap/template.php
@@ -4,3 +4,10 @@
  * @file
  * template.php
  */
+
+// Add ability for Drupal to recognize templates by node _and_ display type.
+// i.e. templates/node--projects--teaser.tpl.php
+function snap_bootstrap_preprocess_node(&$variables) {
+  $variables['theme_hook_suggestions'][] = 'node__' . $variables['type'] . '__' . $variables['view_mode'];
+}
+?>
