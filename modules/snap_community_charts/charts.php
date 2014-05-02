737ee39fe4eb4844dbc570f994335835d2859763
carolynrosner <crosner@alaska.edu>
2014-05-02 11:40:02 -0700

    Fix broken links and edit text in community outlooks section

--- modules/snap_community_charts/charts.php
+++ modules/snap_community_charts/charts.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>SNAP Community Charts</title>
+    <title>SNAP Community Outlooks</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -38,7 +38,7 @@
             <!-- fixed width is important because otherwise variability bars break on chart resize. -->
             <div class="container" style="width: 950px !important;">
 
-                <h1 id="chartsTitle">Community Charts<span id="location"></span></h1>
+                <h1 id="chartsTitle">Community Outlooks<span id="location"></span></h1>
 
                 <div class="row">
 
@@ -47,7 +47,7 @@
                         <div id="variable_selections">
 
                             <div id="comm_select_wrapper">
-                                <p>To load a chart, type your community&rsquo;s name:</p>
+                                <p>Enter a community name to see its climate outlook:</p>
                                 <div class="ui-widget" style="z-index: 1001">
                                     <input type="hidden" id="comm_select_id" value="" name="comm_select_id" />
                                     <input id="comm_select" name="community_selector" type="text" value="" placeholder="Type your community..." />
@@ -57,7 +57,7 @@
                             <div>
 
                                 <div class="inline-controls pull-left">
-                                    <p>Data Set</p>
+                                    <p>Dataset</p>
                                     <div id="variable_buttons" class="buttonset">
                                         <input type="radio" id="variable_temperature" name="dataset" value="1" checked="checked"/><label for="variable_temperature">Temperature</label>
                                         <input type="radio" id="variable_precipitation" name="dataset" value="2" /><label for="variable_precipitation">Precipitation</label>
@@ -102,7 +102,7 @@
 
                     <div class="extent col-md-4" style="position: relative;">
                         <img  src="images/akcanada_extent.gif"/>
-                        <p>SNAP has created charts for communities in the area shown above.</p>
+                        <p>SNAP has created outlooks for communities in the area shown above.</p>
                     </div>
                     
                     <div class="col-md-3">
@@ -121,24 +121,31 @@
 
                     <div id="chart_div"></div>
                 <div class="methods text">
-                    <h4>Interpreting the Community Charts</h4>
-                    <p>
-                        SNAP community charts can be examined for certain key changes and threshold values. Higher mean monthly temperatures in the spring and fall may be of particular interest. This could signify a longer growing season, a loss of ice and/or frozen ground needed for travel or food storage, or a shift in precipitation from snow to rain, which impacts water storage capacity and surface water availability. Warmer, drier spring weather may also be an indicator for increased fire risk. In many locations, winter temperatures are projected to increase dramatically. Warmer winters may allow for the growth of species that are less cold-hardy (including both desirable crops and invasive species), or it may decrease snowpack and increase the frequency of rain-on-snow events that impact wildlife. Higher temperatures across all seasons will likely impact permafrost and land-fast ice.
-                    </p>
+                    <h4>How to interpret climate outlooks for your community</h4>
+<p>You can examine SNAP community outlooks for certain key changes and threshold values—for example, higher mean monthly temperatures in the spring and fall may be of particular interest. This could signify any or all of these conditions:</p>
+<ul>
+  <li>a longer growing season</li>
+  <li>a loss of ice and/or frozen ground needed for travel or food storage</li>
+  <li>a shift in precipitation from snow to rain, which impacts water storage capacity and surface water availability </li>
+</ul>
+<p>Warmer, drier spring weather may also be an indicator for increased fire risk. In many locations, winter temperatures are projected to increase dramatically. Warmer winters may favor growth of species that are less cold-hardy (including  desirable crops and invasive species), or it may decrease snowpack and increase the frequency of rain-on-snow events that impact wildlife. Higher temperatures across all seasons will likely impact permafrost and land-fast ice. </p>
+<h4>How we derived the community outlooks</h4>
+<p><strong>Data sources: </strong>historical <abbr title="Parameter-elevation Regressions on Independent Slopes Model">PRISM</abbr> climatology data and downscaled outputs averaged from five <abbr title="General Circulation Models">GCMs</abbr>. <a href="/methods/downscaling">Learn more about how we downscale climate data from global to regional scales.</a></p>
+<p><strong>We averaged results to smooth out short-term variability. </strong>Results are averaged across decades to lessen the influence of normal year-to-year climate variability on projected values. Averaging also tends to make overall projection trends clearer. Uncertainty is associated with each of these graphed values, and stems from:</p>
+<ul>
+  <li>modeling of atmospheric and oceanic movements used to create <abbr title="General Circulation Models">GCMs</abbr></li>
+  <li>the <abbr title="Parameter-elevation Regressions on Independent Slopes Model">PRISM</abbr> downscaling process></li>
+  <li>the assumptions made regarding greenhouse gas levels for each emissions scenario</li>
+  <li><a href="/methods/uncertainty">Learn more about uncertainty in SNAP&rsquo;s climate research work.</a></li>
+</ul>
+<p><strong>Standard deviations for temperature and precipitation </strong>between the 5 models and averaged over space, time, and the 3 scenarios are relatively small, mostly due to the averaging across space:</p>
+<ul>
+  <li>Precipitation: 5.8 mm - 13.8 mm (mean: 9.8 mm)</li>
+  <li>Temperature: 0.5° C to 2.1° C  (mean: 1.1° C) </li>
+  <li>To compare, standard deviations across the 5 models for a particular spatial pixel could be as much as 500+ mm or &gt;5° C</li>
+</ul>
+<p><strong>Generally, precipitation is more uncertain than temperature.</strong> And, although our models project increases in precipitation, water availability may decrease in some areas due to longer growing seasons and warmer weather.</p>
 
-                    <h4>How the Community Charts Were Derived</h4>
-                    <p>
-                        Information for each community is based on the closest 2 km by 2 km pixel from SNAP's datasets. The charts show historical PRISM climatology data and downscaled outputs averaged from five Global Climate Models (GCMs). Results are also averaged across decades. This averaging lessens the influence of normal year-to-year climate variability on projected values, and tends to make overall projection trends clearer. It is important to note that <a href="/uncertainty.php">uncertainty</a> is associated with each of these graphed values. Uncertainty stems from the modeling of atmospheric and oceanic movements used to create GCMs, from the PRISM <a href="/downscaling.php">downscaling</a> process, and from the assumptions made regarding greenhouse gas levels for each emissions scenario.
-                    </p>
-                    <p>
-                        Standard deviation of precipitation between the five models, averaged over space, time, and the three scenarios, ranges from about 5.8 mm to 13.8 mm with a mean of 9.8 mm.  For temperature, this measure ranges from 0.5 degrees C to 2.1 degrees C with a mean of 1.1 degrees C.  This assessment of variability on a scale that encompasses space, time, and scenarios is relatively small, particularly due to the averaging across space. By comparison, it is important to note that standard deviation across the five models for a particular spatial pixel may be as large as 500+ mm or 5+ degrees C.
-                    </p>
-                    <p>
-                        In general, a higher percentage of uncertainty is associated with precipitation values than with temperature values. It should also be noted that although our models project increases in precipitation, water availability may decrease in some areas due to longer growing seasons and warmer weather.
-                    </p>
-                    <p>
-                        For further information on SNAP projections, please explore our <a href="/methods.php">Methods</a> section.
-                    </p>
                 </div>
         </div>
     </div>
@@ -167,26 +174,29 @@
 
         <div id="processingExportDialog" class="about_dialog" style="display: none;">
             <h4><img src="images/ajax-loader-gray.gif"/> Processing image for export&hellip;</h4>
-            <p>Your download should begin shortly.  High&ndash;resolution PNG images can take up to a minute to process on the server before the download begins.  If you experience problems, please <a href="people.php#contact">let us know</a>.</p>
+            <p>Your download should begin shortly.  High&ndash;resolution PNG images can take up to a minute to process on the server before the download begins.  If you experience problems, please <a href="mailto:tkurkowski@alaska.edu">let us know</a>.</p>
         </div>
 
         <div id="about_scenarios" class="about_dialog" style="display: none;">
-           <p>The Intergovernmental Panel on Climate Change created a range of scenarios to explore alternative development pathways, covering a wide range of demographic, economic and technological driving forces and resulting greenhouse gas emissions.</p>
-           <h4>Emissions leveling and declining (B1)</h4>
-           <p>The B1 scenario describes a convergent world, with the same global population as A1B, but with more rapid changes in economic structures toward a service and information economy.</p>
-
-           <h4>Mid-range emissions (A1B)</h4>
-           <p>The A1B scenario assumes a world of very rapid economic growth, a global population that peaks in mid&ndash;century, rapid introduction of new and more efficient technologies, and a balance between fossil fuels and other energy sources.</p>
-
-           <h4>Rapid increases in emissions (A2)</h4>
-           <p>The A2 scenario describes a very heterogeneous world with high population growth, slow economic development and slow technological change.</p>
+           <p>The <abbr title="Intergovernmental Panel on Climate Change">IPCC</abbr> created  scenarios to explore alternative futures. Scenarios cover a wide range of demographic, economic and technological driving forces, and resulting greenhouse gas emissions.</p>
+<h4>A1B: Mid-range emissions scenario</h4>
+           <p>Very rapid economic growth, a global population that peaks in mid&ndash;century, rapid introduction of new and more efficient technologies, and a balance between fossil fuels and other energy sources.</p>
+           <h4>A2: Rapid increases in emissions</h4>
+           <p>High population growth, but slow economic development and slow technological change.           </p>
+           <h4>B1: Emissions leveling and declining scenario</h4>
+           <p>Same global population as A1B, but with faster changes in economic structures toward a service and information economy.           </p>
 
        </div>
 
        <div id="about_variability" class="about_dialog" style="display: none;">
 
-        <h4>Inter&ndash;Model Variability</h4>
-        <p>Model variability refers to the standard deviation (SD), which provides a measure of dispersion around the mean. The vertical bars represent the SD across the five models. Their lengths represent one SD above and below this value. A small SD indicates the models are in relative agreement, whereas a large SD suggests choice of model is relatively important. Drawing inferences from overlapping or non-overlapping bars is discouraged. The only comparison to make is of their relative size, as it pertains to changes in the degree of agreement among the models.</p>
+        <h4>Inter&ndash;Model variability</h4>
+<p>Model variability refers to the standard deviation (SD), which provides a measure of dispersion around the mean. The vertical bars represent the SD across the 5 models. Their lengths represent one SD above and below this value:</p>
+<ul>
+  <li><strong>Small SD:  </strong>models are in relative agreement</li>
+  <li><strong>Large SD:</strong> choice of model is relatively important</li>
+</ul>
+<p>It is best to avoid drawing inferences from overlapping or non-overlapping bars. The only comparison to make is of their relative size, as it pertains to changes in the degree of agreement among the models.</p>
 
     </div>
     
