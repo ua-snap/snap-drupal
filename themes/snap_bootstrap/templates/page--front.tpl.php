85fb5540eafb3969c44a9a221b63b74634a872eb
carolynrosner <crosner@alaska.edu>
2014-06-18 15:49:31 -0700

    Re-do styles that got overwritten

--- themes/snap_bootstrap/templates/page--front.tpl.php
+++ themes/snap_bootstrap/templates/page--front.tpl.php
@@ -167,7 +167,7 @@
       </section>
 
       <?php if (!empty($page['sidebar_second'])): ?>
-        <aside class="col-sm-3" role="complementary">
+        <aside class="col-md-3 projects" role="complementary">
           <?php print render($page['sidebar_second']); ?>
         </aside>  <!-- /#sidebar-second -->
       <?php endif; ?>
